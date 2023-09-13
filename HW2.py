85e0c57fb66b34cf7c2aa2d7079d04ed4fed431f
bmariswamy5 <bmariswamy5@gwu.edu>
2023-09-13 16:19:53 -0400

    First HW file

--- HW2.py
+++ HW2.py
@@ -9,4 +9,13 @@ print("Number of whitespace in string are:",sum(1 for s in S1 if s.isspace())) #
 
 # HW2 Question 2
 
-S2 = input("Enter the string:\n") # reading the string from user input
+
+S2 = input("Enter the string to be swapped:") # reading the string from user input
+print(S2[1:]+S2[0]) # slice the string from position 1 to end of string and then add the first character to the end of the string
+
+# ----------------------- Question 2 end ----------------------------------------------
+
+# HW2 Question 3
+S3 = input("Enter the name:") #Reading the first,middle and last name from user
+initial="".join([new[0] for new in S3.split()]) #split the  input string and for each strung after split get the character from position 0
+print("The initial are:",initial.upper()) # convert the initial to uppercase and print
