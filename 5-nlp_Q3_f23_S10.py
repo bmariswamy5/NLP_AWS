5e61fb9f7bd270554941907317585c588bca1973
bmariswamy5 <bmariswamy5@gwu.edu>
2023-10-02 17:53:04 -0400

    quiz3

--- 5-nlp_Q3_f23_S10.py
+++ 5-nlp_Q3_f23_S10.py
@@ -13,28 +13,32 @@ that allow you to train and update neural network models on your own data."""
 # -----------------------------------------------------------------------------------------
 # 1:
 # Your code here
+doc = nlp(text)
 
-
-
-
+sentences = list(doc.sents)
+print(f"Number of sentences: {len(sentences)}")
 # -----------------------------------------------------------------------------------------
 # 2
 # Your code here
-
+first_sentence= list(sentences[0])
+print(f"Number of tokens in the first sentence: {len(first_sentence)}")
 
 
 
 # -----------------------------------------------------------------------------------------
 #3
 # Your code here
-
+print("{:<15} {:<10}".format("Token", "POS"))
+for token in first_sentence:
+    print("{:<15} {:<10}".format(token.text, token.pos_))
 
 
 
 # -----------------------------------------------------------------------------------------
 #4
 # Your code here
-
+adj = [token for token in doc if token.sentiment > 0]
+print(f"Number of positive adjectives: {len(adj)}")
 
 
 
@@ -42,5 +46,8 @@ that allow you to train and update neural network models on your own data."""
 #5
 # Your code here
 
-
-
+for i, sentence in enumerate(doc.sents, 1):
+    nouns = [token.text for token in sentence if token.pos_ == 'NOUN']
+    num_nouns = len(nouns)
+    print(f"Sentence {i}: {num_nouns} nouns")
+# I have selected noun feature to count number of noun that occur in the  given sample text
