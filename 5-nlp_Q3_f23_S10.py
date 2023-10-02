163db118f5b3201b6933a727a3e61bc174aeaba5
Brunda Mariswamy <112432842+bmariswamy5@users.noreply.github.com>
2023-10-02 17:28:15 -0400

    quiz 3

---
+++ 5-nlp_Q3_f23_S10.py
@@ -0,0 +1,46 @@
+import spacy
+nlp = spacy.load("en_core_web_sm")
+# -----------------------------------------------------------------------------------------
+text = """SpaCy is a free, open-source library for advanced Natural Language Processing 
+(NLP) in Python. If you're working with a lot of text, you'll eventually want to know 
+more about it. For example, what's it about? What do the words mean in context? 
+Who is doing what to whom? What companies and products are mentioned?
+ Which texts are similar to each other?
+SpaCy helps you answer these questions with a powerful, streamlined API that's easy 
+to use and integrates seamlessly with other Python libraries. SpaCy also comes with 
+pre-trained statistical models and word vectors, and supports deep learning workflows 
+that allow you to train and update neural network models on your own data."""
+# -----------------------------------------------------------------------------------------
+# 1:
+# Your code here
+
+
+
+
+# -----------------------------------------------------------------------------------------
+# 2
+# Your code here
+
+
+
+
+# -----------------------------------------------------------------------------------------
+#3
+# Your code here
+
+
+
+
+# -----------------------------------------------------------------------------------------
+#4
+# Your code here
+
+
+
+
+# -----------------------------------------------------------------------------------------
+#5
+# Your code here
+
+
+
