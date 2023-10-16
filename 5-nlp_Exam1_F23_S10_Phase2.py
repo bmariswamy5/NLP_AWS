ca90916de15b2025e51cdc05adf406d50f8c7f0f
Brunda Mariswamy <112432842+bmariswamy5@users.noreply.github.com>
2023-10-16 15:58:15 -0400

    add

---
+++ 5-nlp_Exam1_F23_S10_Phase2.py
@@ -0,0 +1,31 @@
+#**********************************
+
+
+
+
+
+#**********************************
+#==================================================================================================================================================================
+# Q1:
+"""
+For this question you need to download a text data from the NLTK movie_reviews.
+Use you knowledge that you learned in the class and clean the text appropriately.
+After Cleaning is done, please find the numerical representation of text by any methods that you learned.
+You need to find a creative way to label the sentiment of the sentences.
+This dataset already has positive and negative labels.
+Labeling sentences as 'positive' or 'negative' based on sentiment scores and named then predicted sentiments.
+Create a Pandas dataframe with sentences, true sentiment labels and predicted sentiment labels.
+Calculate the accuracy of your predicted sentiment and true sentiments.
+"""
+#==================================================================================================================================================================
+
+print(20*'-' + 'Begin Q1' + 20*'-')
+
+
+
+
+
+
+
+
+print(20*'-' + 'End Q1' + 20*'-')
